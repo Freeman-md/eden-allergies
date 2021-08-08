ae38507b50d5f2cde54f68232c2b7c6b4be18cb2
Freeman-md <freemanmadudili@gmail.com>
2021-08-08 23:43:44 +0100

    Updated baseURL in bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,7 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-window.axios.defaults.baseURL = 'http://localhost:8000'
+window.axios.defaults.baseURL = 'https://edenallergies.herokuapp.com'
 
 window.axios.interceptors.request.use(
     (config) => {
